134849d43c0d01986bad1523405ae8697899897f
Victor200524 <victor.donaire@hotmail.com>
2025-11-03 14:13:19 -0300

    Primeiro commit

---
+++ RedeNeuralMLP/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module victor.trabalhoredeneural.redeneuralmlp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens victor.trabalhoredeneural.redeneuralmlp to javafx.fxml;
+    exports victor.trabalhoredeneural.redeneuralmlp;
+}
